3fc724c47ad832e9f7aabd163bea24fd421ee452
Avram <ad@dermer.co.il>
2025-07-09 09:09:53 +0300

    day-1

--- main.py
+++ main.py
@@ -54,9 +54,21 @@ def print_tabel(tabel:dict , distance  = 0)-> None:
 
 tabel = aa('./data.csv','Buy_Computer')
 promt = {'age': 'youth', 'income':'high', 'student':'no', 'credit_rating':'excellent'}
-def predict_class(tabel:dict,promt:dict)-> bool:
+def predict_class1(tabel:dict,promt:dict)-> bool:
     yes =  tabel['yes']['age'][promt['age']] * tabel['yes']['income'][promt['income']]*tabel['yes']['student'][promt['student']]*tabel['yes']['credit_rating'][promt['credit_rating']]
     no = tabel['no']['age'][promt['age']]*tabel['no']['income'][promt['income']]*tabel['no']['student'][promt['student']]*tabel['no']['credit_rating'][promt['credit_rating']]
+    print({'no':no,'yes':yes})
     return('no' if no > yes else 'yes')
-print(predict_class(tabel,promt))
 
+def predict_class(tabel:dict,promt:dict)-> bool:
+    dic ={}
+    for k in tabel.keys():
+        num = 1
+        for key,val in promt.items():
+            num *= tabel[k][key][val]
+        dic[k] = num
+    print(dic)
+    return max(dic , key=dic.get)
+
+print(predict_class(tabel,promt))
+print(predict_class1(tabel,promt))
