e85b6e1119683c129360f15fd06796e2fda0badc
Avram <ad@dermer.co.il>
2025-07-22 10:07:46 +0300

    Add Dockerfile with v1.0 options

--- main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-
-# adress = "./data.csv"
-# target = 'Buy_Computer'
-# promt = {'age': 'youth', 'income':'high', 'student':'no', 'credit_rating':'excellent'}
-
-if __name__ == '__main__':
-    uvicorn.run("API:app", host="0.0.0.0", port=8000, reload=True)
